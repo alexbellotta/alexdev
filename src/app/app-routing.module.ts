df208463e868228d6da24757c3e38cb7c79692ed
alexbellotta <alessandrobellotta1998@gmail.com>
2023-11-06 17:47:03 +0100

    feat(header): Add links management

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,26 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeroComponent } from './hero/hero.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { SkillComponent } from './skill/skill.component';
+import { SocialComponent } from './social/social.component';
+import { WorkComponent } from './work/work.component';
 
 const routes: Routes = [
-  {path: 'header-component', component: HeaderComponent},
-  {path: 'app-root', component: AppComponent}
+  {path: '', redirectTo: 'app-hero', pathMatch: 'full' },
+  {path: 'app-header', component: HeaderComponent},
+  {path: 'app-about', component: AboutComponent},
+  {path: 'app-contacts', component: ContactsComponent},
+  {path: 'app-footer', component: FooterComponent},
+  {path: 'app-hero', component: HeroComponent},
+  {path: 'app-projects', component: ProjectsComponent},
+  {path: 'app-skill', component: SkillComponent},
+  {path: 'app-social', component: SocialComponent},
+  {path: 'app-work', component: WorkComponent} 
 ];
 
 @NgModule({
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Link } from '../models/link';
 
 @Component({
   selector: 'app-header',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
+
+  links: Link[] = [
+    {path: 'app-hero', title: 'HOME'},
+    {path: 'app-about', title: 'ABOUT'},
+    {path: 'app-skill', title: 'SKILL'},
+    {path: 'app-work', title: 'WORK'},
+    {path: 'app-projects', title: 'PROJECT'},
+    {path: 'app-contacts', title: 'CONTACTS'},
+  ] 
 
 }
